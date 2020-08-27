5f69f1b4e48fab1b7ecc8abe0ba6879617bc3103
Tahmid Ziko <tahmidziko@gmail.com>
2020-08-27 15:04:06 +0600

    remove credential for demo purpose

--- sms/admin/config/config.php
+++ sms/admin/config/config.php
@@ -2,7 +2,7 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "db_sms");
+define("DB_NAME", "ziko_sms");
 
 define("admin_name", "Admin");
 define("admin_logo", "images/");
