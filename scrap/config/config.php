97d12e7dcfdc4defe39d20b05d3ded46bb2a5895
Tahmid Ziko <tahmidziko@gmail.com>
2020-08-27 14:29:25 +0600

    Scrap Projects Config file Change

--- scrap/config/config.php
+++ scrap/config/config.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "tahmidzi_admin");
-define("DB_PASS", "e8yV#m4)Oib1");
-define("DB_NAME", "tahmidzi_db_blog");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "ziko_scrap");
 
 define("company_name", "Web Scrap Using PHP");
 define("company_logo_url", "images/logo.png");
